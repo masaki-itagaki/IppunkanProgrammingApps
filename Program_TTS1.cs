f7487ff9cded241ca490bd2a911876c05ed3d5a2
masaki-itagaki <52015725+masaki-itagaki@users.noreply.github.com>
2019-12-26 16:34:27 -0800

    Add files via upload
    This is the first sample code.

---
+++ Program_TTS1.cs
@@ -0,0 +1,12 @@
+﻿using OneMinCoding.Speech;
+
+namespace IppunkanProgrammingApps
+{
+    class Program_TTS1
+    {
+        static void Main(string[] args)
+        {
+            VoiceWin.Speak("This is my first speech.", "en-US");
+        }
+    }
+}
