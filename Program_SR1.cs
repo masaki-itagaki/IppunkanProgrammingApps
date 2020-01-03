eefcd01c6f41a2910a1467a755a03dc14032d334
masaki-itagaki <52015725+masaki-itagaki@users.noreply.github.com>
2020-01-02 16:06:27 -0800

    Test program for "Recognize" method

---
+++ Program_SR1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OneMinCoding.Speech;
+
+namespace IppunkanProgrammingApps
+{
+    class Program_SR1
+    {
+        static void Main(string[] args)
+        {
+            VoiceWin.Speak("Say something after the beep.", "en-US");
+            Console.Beep();
+            string reco = VoiceWin.Recognize("en-US"); 
+            Console.WriteLine(reco);
+
+            Console.ReadLine();
+        }
+    }
+}
