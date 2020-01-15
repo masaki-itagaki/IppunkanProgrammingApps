435ec88689b51348f0ab6d0439422ddfcef5a72d
masaki-itagaki <52015725+masaki-itagaki@users.noreply.github.com>
2020-01-14 22:14:54 -0800

    ChatTrainer Step 3 - Chat.txtの各行を表示するコードを変更

---
+++ Program_CT3.cs
@@ -0,0 +1,56 @@
+﻿using OneMinCoding.Speech;
+using System;
+using System.IO;
+using System.Text;
+using OneMinCoding.Speech;
+
+namespace ChatTrainer
+{
+    class Program_CT3
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding("Shift-JIS");
+            //===========================
+            //Step 1: アプリ始動時のユーザー指示
+            //===========================
+            //モード選択の指示
+            string modeInstruction = "トレーニングモードを選択してください: ";
+            Console.WriteLine(modeInstruction); //画面表示
+            VoiceWin.Speak(modeInstruction, "ja-JP");//音声指示
+
+            //モードの紹介
+            Console.WriteLine("=======================");
+            Console.WriteLine("1: 会話を聴くだけ");
+            Console.WriteLine("AかB: AかBの話者を練習");
+            Console.WriteLine("2: ランダムトレーニング");
+            Console.WriteLine("3: 会話ポイントゲーム");
+            Console.WriteLine("0: 発音繰り返しトレーニング");
+            Console.WriteLine("=======================/n");
+            Console.Write("番号を入力：");
+
+            //選択番号をmodeに入れる。大文字化する。
+            string mode = Console.ReadLine().ToUpper();
+
+            //===========================
+            //Step 2: トレーニング開始
+            //===========================
+
+            //Chat.txtを読む。すべての行が文字列配列のlinesに。
+            string[] lines = File.ReadAllLines("Chat.txt", Encoding.Unicode);
+
+            //英文をキープする入れ物
+            string English = string.Empty;
+
+            //linesを一行ずつ読む
+            foreach (string line in lines)
+            {
+                //各行を音声と画面表で出す。
+                VoiceWin.ReadChatLine(line, true, true, out English);
+            }
+
+            //コンソール画面をホールドする
+            Console.ReadLine();
+        }
+    }
+}
