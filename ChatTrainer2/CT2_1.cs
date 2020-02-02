ac6d854394246cae5d4781d7e41ed580cb986a7c
masaki-itagaki <52015725+masaki-itagaki@users.noreply.github.com>
2020-02-02 11:33:58 -0800

    クラウドの音声合成サービスを塚つためのテストコード
    まずは.Net Coreのコンソールアプリを作って、ここのコードを試してください。Azureの音声サービスのサブスクリプションキーが必要ですので、解説に従って取得してください。
    【無料のAPIキーの取得方法】
    https://azure.microsoft.com/ja-jp/try/cognitive-services/?api=speech-services

---
+++ ChatTrainer2/CT2_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using IppunCore.Speech;
+
+namespace CT2_1
+{
+    class Program
+    {
+        //自分で取得したAzure音声サービスのサブスクリプションキー（APIキー）を指定。
+        //二重引用符の中にキーの文字列をコピペします
+        const string subKeyJ = "ここに自分のAPIキーを入れる"; 
+
+        static void Main(string[] args)
+        {
+            //日本語と英語で話すテキストを文字列変数、textJとtextEに入れる
+            string textJ = "何か英語で話してください";
+            string textE = "Sure. This is my first computer voice.";
+            
+            //まずは日本語を話す。Regionには"japaneast"を指定。
+            Voice.SpeakJapanese(textJ, "Haruka", subKeyJ, "japaneast");
+            //次に英語を話す。
+            Voice.SpeakEnglish(textE, "Ben", subKeyJ, "japaneast");
+
+            //注：Vocei名は男女の声別にいくつか選択できます。
+            //日本語のVoiceNameは、Haruka, Ayumi, Ichiroの３つ
+            //英語のVoiceNameは、Ben, Guy, Jessa, Ziraの４つから選ぶ
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
